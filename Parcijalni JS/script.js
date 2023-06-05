83cbec94f81d13f8653847b71f4d42a7d73ed024
Patrik Posavec <38071879+prezvust@users.noreply.github.com>
2023-06-05 21:38:21 +0200

    script.js

--- Parcijalni JS/script.js
+++ Parcijalni JS/script.js
@@ -1,66 +1,91 @@
 "use strict";
 
-document.addEventListener('DOMContentLoaded', function() {
+const searchInput = document.getElementById('searchInput');
+const loader = document.getElementById('loader');
+const resultsContainer = document.getElementById('resultsContainer');
+const noResultsMessage = document.createElement('div');
+noResultsMessage.id = 'noResultsMessage';
+noResultsMessage.textContent = 'Ne postoji pjesma ni izvođač pod tim unosom. Pokušajte ispočetka.';
 
-    const searchInput = document.getElementById('searchInput');
-    const searchButton = document.getElementById('searchButton');
-    const resultsContainer = document.getElementById('results');
-    const loader = document.getElementById('loader');
+let timerId;
+
+searchInput.addEventListener('input', () => {
+  clearTimeout(timerId);
+  timerId = setTimeout(fetchData, 500);
+});
+
+function fetchData() {
+  const searchTerm = searchInput.value.trim();
+  if (searchTerm === '') {
+    displayNoResultsMessage();
+    return;
+  }
+
+  showLoader();
+
   
-   
-    searchButton.addEventListener('click', handleSearch);
-  
-    function handleSearch() {
-      const query = searchInput.value.trim();
-  
-      
-      resultsContainer.innerHTML = '';
-  
-      if (query.length > 0) {
-        showLoader();
-  
-       
-        const searchUrl = `https://itunes.apple.com/search?term=${encodeURIComponent(query)}`;
-  
-       
-        fetch(searchUrl)
-          .then(response => response.json())
-          .then(data => {
-            hideLoader();
-  
-            if (data.resultCount === 0) {
-              displayMessage('Nema rezultata.');
-            } else {
-              displayResults(data.results);
-            }
-          })
-          .catch(error => {
-            hideLoader();
-            displayMessage('Dogodila se pogreška. Molimo pokušajte ponovno kasnije.');
-          });
+  const url = `https://itunes.apple.com/search?term=${encodeURIComponent(searchTerm)}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      hideLoader();
+
+      if (data.results.length > 0) {
+        displayResults(data.results);
+      } else {
+        displayNoResultsMessage();
       }
-    }
-  
-    function showLoader() {
-      loader.style.display = 'block';
-    }
-  
-    function hideLoader() {
-      loader.style.display = 'none';
-    }
-  
-    function displayMessage(message) {
-      const messageElement = document.createElement('p');
-      messageElement.textContent = message;
-      resultsContainer.appendChild(messageElement);
-    }
-  
-    function displayResults(results) {
-      results.forEach(result => {
-        const resultElement = document.createElement('p');
-        resultElement.textContent = result.trackName;
-        resultsContainer.appendChild(resultElement);
-      });
-    }
-  });
-  
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      hideLoader();
+      displayNoResultsMessage();
+    });
+}
+
+function showLoader() {
+  loader.style.display = 'block';
+  resultsContainer.style.display = 'none';
+  noResultsMessage.style.display = 'none';
+}
+
+function hideLoader() {
+  loader.style.display = 'none';
+}
+
+function displayResults(results) {
+  const table = document.createElement('table');
+  const tableHeader = table.createTHead();
+  const headerRow = tableHeader.insertRow();
+  const th1 = document.createElement('th');
+  th1.textContent = 'Pjesma';
+  const th2 = document.createElement('th');
+  th2.textContent = 'Izvođač';
+  headerRow.appendChild(th1);
+  headerRow.appendChild(th2);
+
+  const tableBody = table.createTBody();
+  for (const result of results) {
+    const row = tableBody.insertRow();
+    const cell1 = row.insertCell();
+    cell1.textContent = result.trackName;
+    const cell2 = row.insertCell();
+    cell2.textContent = result.artistName;
+  }
+
+  resultsContainer.innerHTML = '';
+  resultsContainer.appendChild(table);
+  resultsContainer.style.display = 'block';
+  noResultsMessage.style.display = 'none';
+}
+
+function displayNoResultsMessage() {
+  resultsContainer.innerHTML = '';
+  resultsContainer.style.display = 'none';
+  noResultsMessage.style.display = 'block';
+  if (!noResultsMessage.parentNode) {
+    document.body.appendChild(noResultsMessage);
+  }
+}
+
